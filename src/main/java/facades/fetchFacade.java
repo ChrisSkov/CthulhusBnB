fb0b76140a55092cd8ed362e69730936e632c439
# Created by https://www.gitignore.io/api/windows,netbeans,java-web <cph-ss451@cphbusiness.dk>
2019-11-20 14:30:42 +0100

    n

--- src/main/java/facades/fetchFacade.java
+++ src/main/java/facades/fetchFacade.java
@@ -99,7 +99,7 @@ public class fetchFacade {
     {
         //Changes
         fetchFacade facade = new fetchFacade();
-        
+        //
         String result = facade.fetch("https://cthulhusbnb.herokuapp.com/Rooms");
         System.out.println(result);
     }
