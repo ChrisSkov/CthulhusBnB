3220c9502c4ee4de6fd969e55c361475e914041b
Chris <Stofferdyr@gmail.com>
2019-11-20 12:45:44 +0100

    maybe?

--- jsonServers/server2.js
+++ jsonServers/server2.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-//const router = jsonServer.router('db2.json');
+//const router = jsonServer.router('db.json');
+const router = jsonServer.router('db2.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.use(middlewares);
 server.use(router);
