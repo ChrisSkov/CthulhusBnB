84737489b9e3782f145cb054bf00c53a9ffd745e
Chris <Stofferdyr@gmail.com>
2019-11-23 22:07:51 +0100

    start adding relations between classes
    i have no idea what im doing

--- src/main/java/entities/Country.java
+++ src/main/java/entities/Country.java
@@ -6,10 +6,13 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -22,6 +25,8 @@ public class Country implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "hotel")
+    private List<Hotel> hotels;
 
     public int getId()
     {
--- src/main/java/entities/Hotel.java
+++ src/main/java/entities/Hotel.java
@@ -6,10 +6,13 @@
 package entities;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -22,6 +25,10 @@ public class Hotel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Country country;
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "hotel")
+    private Room room;
 
     public int getId()
     {
@@ -62,5 +69,5 @@ public class Hotel implements Serializable {
     {
         return "entities.Hotel[ id=" + id + " ]";
     }
-    
+
 }
